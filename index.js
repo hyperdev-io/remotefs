fd13db02b6e1c2e31962fcac694f03910b6656a6
Gantcho Kojuharov <gantcho@greyarch.net>
2016-11-18 16:43:01 +0100

    added /fs/du endpoint

--- index.js
+++ index.js
@@ -12,25 +12,54 @@ const baseDir = process.env.BASE_DIR;
 assert(port, "PORT must be defined");
 assert(baseDir, "BASE_DIR must be defined");
 
-copy = function(req, res, next) {
-  const source = req.body.source;
-  const destination = req.body.destination;
-  if (!(source && destination)) {
-    return next(new Error("Both source and destination are required fields"));
-  } else {
-    return child_process.exec("cp -rp " + source + " " + destination, function(err, out, code) {
+function resolve(dir) {
+  return `${baseDir}${path.resolve(dir)}`;
+};
+
+function cp({body: {source, destination}}, res, next) {
+  if (source && destination) {
+    // path.resolve the parameters to prevent escaping from the base dir with ..
+    source = resolve(source);
+    destination = resolve(destination);
+    child_process.exec(`cp -rp ${source} ${destination}`, (err, out, code) => {
       if (err) {
         console.error(err);
         return next(err);
       } else {
-        res.json(req.body);
+        res.json({
+          source: source,
+          destination: destination
+        });
         return next();
       }
     });
-  }
+  } else {
+    return next(new Error("Both 'source' and 'destination' are required fields"));
+  };
 };
 
-server.post('/fs/cp', copy);
+function du({body: {dir}}, res, next) {
+  if (dir) {
+    // path.resolve the parameters to prevent escaping from the base dir with ..
+    dir = resolve(dir);
+    child_process.exec(`du -sb #{dir} | awk '{ print $1 }'`, (err, out, code) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      } else {
+        res.json({
+          size: parseInt(out)
+        });
+        return next();
+      }
+    });
+  } else {
+    return next(new Error("'dir' is a required field"));
+  };
+};
+
+server.post('/fs/cp', cp);
+server.post('/fs/du', du);
 
 server.listen(port, function() {
   return console.log("remotefs listening " + server.url);

fc86b8c7112afe84941dc052c6989bcf6d82df7b
Greyarch BV <gantcho@greyarch.net>
2017-09-26 10:36:48 +0200

    parameterize polling intervals

--- src/index.js
+++ src/index.js
@@ -13,10 +13,14 @@ const datastore = require("./datastore");
 const buckets = require("./buckets");
 const remotefs = require("./remotefs");
 
-const port = process.env.PORT;
-const baseDir = process.env.BASE_DIR;
-
 module.exports = () => {
+  const port = process.env.PORT;
+  const baseDir = process.env.BASE_DIR;
+  const listBucketsInterval = process.env.LIST_BUCKETS_INTERVAL || 2000;
+  const getBucketSizesInterval = process.env.GET_BUCKET_SIZES_INTERVAL || 30000;
+  const getDatastoreSizeInterval =
+    process.env.GET_DATASTORE_SIZE_INTERVAL || 10000;
+
   assert(port, "PORT must be defined");
   assert(baseDir, "BASE_DIR must be defined");
 
@@ -25,14 +29,17 @@ module.exports = () => {
 
   const publishBucketsInfo = buckets =>
     mqttClient.publish("/agent/storage/buckets", buckets, { retain: true });
-  const listBuckets = _.debounce(buckets.list(baseDir, events, false), 1000);
+  const listBuckets = _.debounce(
+    buckets.list(baseDir, events, false),
+    listBucketsInterval
+  );
   events.on("/bucket/**", listBuckets);
   events.on("/buckets/changed", publishBucketsInfo);
 
-  setInterval(buckets.list(baseDir, events), 30000);
+  setInterval(buckets.list(baseDir, events), getBucketSizesInterval);
   setInterval(
     datastore.publishDataStoreUsage(mqttClient, "/agent/storage/size", baseDir),
-    10000
+    getDatastoreSizeInterval
   );
 
   const rfs = remotefs(baseDir, events);
